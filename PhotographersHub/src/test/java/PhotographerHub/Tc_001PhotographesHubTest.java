9f97c8075fb0a6e08962e4190029830c0c1a5f43
pooja.bangera <pooja.bangera@INBL01LAP20091.brd.g-c-i.com>
2022-10-13 13:01:18 +0530

    First Commit

---
+++ PhotographersHub/src/test/java/PhotographerHub/Tc_001PhotographesHubTest.java
@@ -0,0 +1,12 @@
+package PhotographerHub;
+
+import org.testng.annotations.Test;
+
+import genericLibrary.Base_Class;
+
+public class Tc_001PhotographesHubTest extends Base_Class{
+@Test
+public void Photographer() {
+	
+}
+}
